11afd70947feaaa68ac8b9c3378209e0e164a83a
Vinod V <vinodvij82@gmail.com>
2016-04-04 02:01:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# misc_scala_spark
---
+++ artist_movies.scala
@@ -0,0 +1,15 @@
+val artistsDF             = sqlContext.read.json("/home/hduser/artists_en.json");
+val moviesDF              = sqlContext.read.json("/home/hduser/movies_en.json");
+val moviesDFnoDesc        = moviesDF.select("actors","country","director","genre","id","title","year");
+
+val no_of_movies_per_year = grpmovie_year.agg(count("title")).toDF("Year","No of Movies");
+
+
+moviesDF.join(artistsDF,moviesDF("actors.id").cast(StringType).contains(artistsDF("id"))).select("first_name","last_name","title").show(200)
+
+
+
+
+
+
+
