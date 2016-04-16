196c108850f60f385cdafb8a69d2fac9dd83f269
Vinod Vijayan <vinodvij82@gmail.com>
2016-04-16 13:04:47 +0530

    Update file_word_count.scala

--- file_word_count.scala
+++ file_word_count.scala
@@ -12,4 +12,16 @@ val maplineRDD = sc.textFile("/home/hduser/IdeaProjects/files/ham.txt")
                 // Map each row to (word, frequency of word)
                 .map( w => (w._1, w._2.size))
                 // Sort by word frequency in descending order and take only top 10 frequency words
-                .sortBy(_._2,false).collect.take(10)
+                .sortBy(_._2,false)
+                .collect
+                .take(10)
+
+// Another way using reduceByKey
+val maplineRRD = sc.textFile("/home/hduser/IdeaProjects/files/ham.txt")
+                .flatMap(_.split("\\W+"))
+                .filter(_.trim.length >0)
+                .map( w => (w,1))
+                .reduceByKey(_ + _)
+                .sortBy(_._2,false)
+                .collect
+                .take(10)
