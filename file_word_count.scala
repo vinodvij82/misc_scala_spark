96d90f2ffcd3d4a6bba91e1c016df6ac8d87c05b
Vinod Vijayan <vinodvij82@gmail.com>
2016-04-16 12:37:11 +0530

    Create file_word_count.scala

---
+++ file_word_count.scala
@@ -0,0 +1,15 @@
+
+
+
+
+val maplineRDD = sc.textFile("/home/hduser/IdeaProjects/files/ham.txt")
+                // Split each line to individual words and create an list of all words in file
+                .flatMap(_.split("\\W+"))     
+                // Filter out the empty words
+                .filter(_.trim().length > 0)  
+                // Groupby word so now each row will be word1, Iterable(word1,word1,word1), cosidering word1 occurs 3 times
+                .groupBy( w => w)             
+                // Map each row to (word, frequency of word)
+                .map( w => (w._1, w._2.size))
+                // Sort by word frequency in descending order and take only top 10 frequency words
+                .sortBy(_._2,false).collect.take(10)
